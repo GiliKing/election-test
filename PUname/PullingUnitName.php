ccd83e8c3008a4aa4503f86ede8d36c74a2fe86a
crixtiangili <ogilichristianonyedika@gmail.com>
2022-03-05 15:03:18 -0800

    first commit

--- PUname/PullingUnitName.php
+++ PUname/PullingUnitName.php
@@ -5,86 +5,77 @@
 
 require '../database/connect.php';
 
+$puName = "SELECT * FROM `polling_unit`";
+$result = mysqli_query($conn, $puName);
 
-if($conn) {
+if($result) {
 
-    session_start();
+    echo '<table class="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th>S/N </th>
+                        <th>Polling Unit Name</th>
+                        <th>Polling Unit Number</th>
+                        <th>Click to show result</th>
+                    </tr>
 
-    $puName = "SELECT * FROM `polling_unit`";
-    $result = mysqli_query($conn, $puName);
+                </thead>
+                <tbody>
+            ';
 
-    if($result) {
-
-        echo '<table class="table table-striped table-dark">
-                    <thead>
-                        <tr>
-                            <th>S/N </th>
-                            <th>Polling Unit Name</th>
-                            <th>Polling Unit Number</th>
-                            <th>Click to show result</th>
-                        </tr>
-
-                    </thead>
-                    <tbody>
-                ';
-
-        $i = 1;
-        while( $row= mysqli_fetch_array($result, MYSQLI_ASSOC) )
-        {
-            $number = $row['polling_unit_number'];
-            $name = $row['polling_unit_name'];
-            $uniqueID = $row['uniqueid'];
-            
-            
-
-            if($name != null) {
-
-                echo'
-                <tr>
-                <td><p ="make_big">'.$i.'</p></td>
-                <td><p ="make_big">'.$name.'</p></td>
-                <td><p ="make_big">'.$number.'</p></td>
-                <td>
-                <input type="hidden" id="make'.$i.'" value='.$uniqueID.'>
-                <button class="btn btn-primary btn-lg btn-block poll_btn'.$i.'">Show Result</button>
-                <script>
-                        document.querySelector(".poll_btn'.$i.'").addEventListener("click", () => {
-
-                            let id = document.getElementById("make'.$i.'").value.trim();
-
-                            $.ajax({
-                                url:"../process/forms.php",
-                                method: "POST",
-                                data: {
-                                    ok: id,
-                                },
-                                success: function(data) {
-                                    if(data.trim() == "yes") {
-                                        window.location.href = "../PUresults/PollingUnitResult.php?id='.base64_encode($uniqueID).'";
-                                    }
-                                }
-                            })
-                        })
-                </script>
-                </td>
-                </tr>
-                ';
-
-            }
-
-
-
-            $i++;
-        }
-
-        echo "
-            </tbody>
-            </table>
-        ";
-
+    $i = 1;
+    while( $row= mysqli_fetch_array($result, MYSQLI_ASSOC) )
+    {
+        $number = $row['polling_unit_number'];
+        $name = $row['polling_unit_name'];
+        $uniqueID = $row['uniqueid'];
+        
         
 
+        if($name != null) {
+
+            echo'
+            <tr>
+            <td><p ="make_big">'.$i.'</p></td>
+            <td><p ="make_big">'.$name.'</p></td>
+            <td><p ="make_big">'.$number.'</p></td>
+            <td>
+            <input type="hidden" id="make'.$i.'" value='.$uniqueID.'>
+            <button class="btn btn-primary btn-lg btn-block poll_btn'.$i.'">Show Result</button>
+            <script>
+                    document.querySelector(".poll_btn'.$i.'").addEventListener("click", () => {
+
+                        let id = document.getElementById("make'.$i.'").value.trim();
+
+                        $.ajax({
+                            url:"../process/forms.php",
+                            method: "POST",
+                            data: {
+                                ok: id,
+                            },
+                            success: function(data) {
+                                if(data.trim() == "yes") {
+                                    window.location.href = "../PUresults/PollingUnitResult.php?id='.base64_encode($uniqueID).'";
+                                }
+                            }
+                        })
+                    })
+            </script>
+            </td>
+            </tr>
+            ';
+
+        }
+
+
+
+        $i++;
     }
+
+    echo "
+        </tbody>
+        </table>
+    ";
 
 
 }
